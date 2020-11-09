d6a69030b6869a0f030a8b3d215269254f28e053
Arnold Cytrowski <cytrowskiarnold@gmail.com>
2020-11-09 11:11:52 +0100

    some updates

--- simplestopwatch.py
+++ simplestopwatch.py
@@ -1,10 +1,11 @@
 #! python
 # simplestopwatch.py - very simple stopwatch, absolutely for free
+# script requires pyperclyp module
 # XI 2020 Arnold Cytrowski
 
 
 
-import time
+import time, pyperclip
 
 print('Press <ENTER> to start. Press <ENTER> again for starting new lap')
 print('Press <CTRL + C> for finish the program')
@@ -21,7 +22,13 @@ try:
         total_time = round(time.time() - start_time, 2)
         print(f'Lap#{lap_num}: total time:{total_time}, laptime:{lap_time}', end='')
 
+        lap = 'lap # {} {} ({})'.format((str(lap_num)+ ':').ljust(3),
+                                         str(total_time).rjust(5),
+                                         str(lap_time).rjust(6))
+        print(lap, end='')
+
         lap_num += 1
         last_time = time.time()
+        pyperclip.paste(lap)
 except KeyboardInterrupt:
     print('And it\'s done')
